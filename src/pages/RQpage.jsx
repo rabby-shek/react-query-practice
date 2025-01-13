d1b48a34303b40951a865681e2eb79ef1544dad9
rabby-shek <rabbyshek1998@gmail.com>
2025-01-13 13:05:40 +0600

    learning useQuery and useMutation

--- src/pages/RQpage.jsx
+++ src/pages/RQpage.jsx
@@ -1,29 +1,11 @@
-import React from "react";
-import { useQuery } from "react-query";
-import axios from "axios";
-import useUsersData from "../hooks/useUsersData";
-import { NavLink } from "react-router-dom";
+import React from 'react'
 
 const RQpage = () => {
-  const { data, isLoading, isFetching } = useUsersData();
-
-  if (isLoading) {
-    return <h2>Loading...</h2>;
-  }
-
-  console.log(isFetching);
   return (
     <div>
-
-      {data?.data.map((user) => {
-        return (
-          <div key={user.id}>
-            <NavLink to={`/details/${user.id}`}>{user.name}</NavLink>
-          </div>
-        );
-      })}
+      aas
     </div>
-  );
-};
+  )
+}
 
-export default RQpage;
+export default RQpage
